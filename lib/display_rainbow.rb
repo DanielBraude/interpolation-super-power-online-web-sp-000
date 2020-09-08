3371d51ae2df2e90c98bb5d6496e29cb3cbbc62c
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-09-08 23:01:22 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,6 +4,7 @@ puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
 puts "Contact: #{person[3]}"
 end
 
+stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
 bob = ["Bob", 46, "Juggler", "555-555-5555"]
 
 print_business_card(bob)
